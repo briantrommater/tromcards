a91610bd5707aa9514819442c40ad155e7b93927
brian.trommater <eightysixhearts@yahoo.com>
2018-10-15 15:58:18 -0400

    moregames&difftiming

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
 });
 
 let deckOfCards = ['2C', '2H', '2S', '2D', '3C', '3H', '3S', '3D', '4C', '4H', '4S', '4D',
-'5C', '5H', '5S', '5D', '6C', '6H', '6S', '7C', '7H', '7S', '7D', '8C', '8H', '8S', '8D',
+'5C', '5H', '5S', '5D', '6C', '6H', '6S', '6D','7C', '7H', '7S', '7D', '8C', '8H', '8S', '8D',
 '9C', '9H', '9S', '9D', '10C', '10H', '10S', '10D', 'JC', 'JH', 'JS', 'JD', 'QC', 'QH',
 'QS', 'QD', 'KC', 'KH', 'KS', 'KD', 'AC', 'AH', 'AS', 'AD'];
 
@@ -12,10 +12,15 @@ let hearts = ['2H', '3H', '4H', '5H', '6H', '7H', '8H', '9H', '10H', 'JH', 'QH',
 let spades = ['2S', '3S', '4S', '5S', '6S', '7S', '8S', '9S', '10S', 'JS', 'QS', 'KS', 'AS'];
 let clubs = ['2C', '3C', '4C', '5C', '6C', '7C', '8C', '9C', '10C', 'JC', 'QC', 'KC', 'AC'];
 
-let halfDeckRed = ['2D', '3D', '4D', '5D', '6D', '7D', '8D', '9D', '10D', 'JD', 'QD', 'KD', 'AD','2H', '3H', '4H', '5H', '6H', '7H', '8H', '9H', '10H', 'JH', 'QH', 'KH', 'AH'];
-let halfDeckBlack = ['2S', '3S', '4S', '5S', '6S', '7S', '8S', '9S', '10S', 'JS', 'QS', 'KS', 'AS', '2C','3C', '4C', '5C', '6C', '7C', '8C', '9C', '10C', 'JC', 'QC', 'KC', 'AC'];
-let halfDeckDC = ['2D', '3D', '4D', '5D', '6D', '7D', '8D', '9D', '10D', 'JD', 'QD', 'KD', 'AD', '2C', '3C', '4C', '5C', '6C', '7C', '8C', '9C', '10C', 'JC', 'QC', 'KC', 'AC'];
-let halfDeckSH = ['2S', '3S', '4S', '5S', '6S', '7S', '8S', '9S', '10S', 'JS', 'QS', 'KS', 'AS', '2H', '3H', '4H', '5H', '6H', '7H', '8H', '9H', '10H', 'JH', 'QH', 'KH', 'AH'];
+let halfDeckRed = ['2D', '3D', '4D', '5D', '6D', '7D', '8D', '9D', '10D', 'JD', 'QD', 'KD',
+'AD','2H', '3H', '4H', '5H', '6H', '7H', '8H', '9H', '10H', 'JH', 'QH', 'KH', 'AH'];
+let halfDeckBlack = ['2S', '3S', '4S', '5S', '6S', '7S', '8S', '9S', '10S', 'JS', 'QS', 'KS',
+'AS', '2C', '3C', '4C', '5C', '6C', '7C', '8C', '9C', '10C', 'JC', 'QC', 'KC', 'AC'];
+let singleDigits = ['2C', '2H', '2S', '2D', '3C', '3H', '3S', '3D', '4C', '4H', '4S', '4D',
+'5C', '5H', '5S', '5D', '6C', '6H', '6S', '6D', '7C', '7H', '7S', '7D', '8C', '8H', '8S', '8D',
+'9C', '9H', '9S', '9D'];
+let broadways = ['10C', '10H', '10S', '10D', 'JC', 'JH', 'JS', 'JD', 'QC', 'QH', 'QS', 'QD',
+'KC', 'KH', 'KS', 'KD', 'AC', 'AH', 'AS', 'AD'];
 
 let clonedCards = [];
 
@@ -23,34 +28,219 @@ let missingCard;
 
 
 
-function quarter() {
+function club() {
+    clonedCards = [...clubs];
+    removeCard()
+    $('.carousel').carousel('dispose')
+    $('.carousel').carousel({
+        interval: 400
+    })
+    let theseDisappear = document.getElementsByClassName("start");
+        for (let thing of theseDisappear) {
+           thing.style.display = "none";
+    }
+    $('.carousel').on('slid.bs.carousel', function (e) {
+        let num_cards = $('.carousel-item').length
+        if ( (num_cards - 1) === e.to) {
+            $('.carousel-inner').empty();
+            let fiftyTwoAppear = document.getElementsByClassName("containerA");
+                for (let stuff of fiftyTwoAppear) {
+                    stuff.style.display = "block";
+                }
+        }
+    })
+}
+function diamond() {
     clonedCards = [...diamonds];
     removeCard()
+    $('.carousel').carousel('dispose')
+    $('.carousel').carousel({
+        interval: 400
+    })
     let theseDisappear = document.getElementsByClassName("start");
-    for (let thing of theseDisappear) {
-        thing.style.display = "none";
+        for (let thing of theseDisappear) {
+           thing.style.display = "none";
     }
+    $('.carousel').on('slid.bs.carousel', function (e) {
+        let num_cards = $('.carousel-item').length
+        if ( (num_cards - 1) === e.to) {
+            $('.carousel-inner').empty();
+            let fiftyTwoAppear = document.getElementsByClassName("containerA");
+                for (let stuff of fiftyTwoAppear) {
+                    stuff.style.display = "block";
+                }
+        }
+    })
 }
-function half() {
-    clonedCards = [...halfDeckSH];
+function heart() {
+    clonedCards = [...hearts];
     removeCard()
+    $('.carousel').carousel('dispose')
+    $('.carousel').carousel({
+        interval: 400
+    })
     let theseDisappear = document.getElementsByClassName("start");
-    for (let thing of theseDisappear) {
-        thing.style.display = "none";
+        for (let thing of theseDisappear) {
+           thing.style.display = "none";
     }
-
+    $('.carousel').on('slid.bs.carousel', function (e) {
+        let num_cards = $('.carousel-item').length
+        if ( (num_cards - 1) === e.to) {
+            $('.carousel-inner').empty();
+            let fiftyTwoAppear = document.getElementsByClassName("containerA");
+                for (let stuff of fiftyTwoAppear) {
+                    stuff.style.display = "block";
+                }
+        }
+    })
 }
+function spade() {
+    clonedCards = [...spades];
+    removeCard()
+    $('.carousel').carousel('dispose')
+    $('.carousel').carousel({
+        interval: 400
+    })
+    let theseDisappear = document.getElementsByClassName("start");
+        for (let thing of theseDisappear) {
+           thing.style.display = "none";
+    }
+    $('.carousel').on('slid.bs.carousel', function (e) {
+        let num_cards = $('.carousel-item').length
+        if ( (num_cards - 1) === e.to) {
+            $('.carousel-inner').empty();
+            let fiftyTwoAppear = document.getElementsByClassName("containerA");
+                for (let stuff of fiftyTwoAppear) {
+                    stuff.style.display = "block";
+                }
+        }
+    })
+}
+function halfDeckR() {
+    clonedCards = [...halfDeckRed];
+    removeCard()
+    $('.carousel').carousel('dispose')
+    $('.carousel').carousel({
+        interval: 1200
+    })
+    let theseDisappear = document.getElementsByClassName("start");
+        for (let thing of theseDisappear) {
+           thing.style.display = "none";
+    }
+    $('.carousel').on('slid.bs.carousel', function (e) {
+        let num_cards = $('.carousel-item').length
+        if ( (num_cards - 1) === e.to) {
+            $('.carousel-inner').empty();
+            let fiftyTwoAppear = document.getElementsByClassName("containerA");
+                for (let stuff of fiftyTwoAppear) {
+                    stuff.style.display = "block";
+                }
+        }
+    })
+}
+function halfDeckB() {
+    clonedCards = [...halfDeckBlack];
+    removeCard()
+    $('.carousel').carousel('dispose')
+    $('.carousel').carousel({
+        interval: 1200
+    })
+    let theseDisappear = document.getElementsByClassName("start");
+        for (let thing of theseDisappear) {
+           thing.style.display = "none";
+    }
+    $('.carousel').on('slid.bs.carousel', function (e) {
+        let num_cards = $('.carousel-item').length
+        if ( (num_cards - 1) === e.to) {
+            $('.carousel-inner').empty();
+            let fiftyTwoAppear = document.getElementsByClassName("containerA");
+                for (let stuff of fiftyTwoAppear) {
+                    stuff.style.display = "block";
+                }
+        }
+    })
+}
+function number() {
+    clonedCards = [...singleDigits];
+    removeCard()
+    $('.carousel').carousel('dispose')
+    $('.carousel').carousel({
+        interval: 1200
+    })
+    let theseDisappear = document.getElementsByClassName("start");
+        for (let thing of theseDisappear) {
+           thing.style.display = "none";
+    }
+    $('.carousel').on('slid.bs.carousel', function (e) {
+        let num_cards = $('.carousel-item').length
+        if ( (num_cards - 1) === e.to) {
+            $('.carousel-inner').empty();
+            let fiftyTwoAppear = document.getElementsByClassName("containerA");
+                for (let stuff of fiftyTwoAppear) {
+                    stuff.style.display = "block";
+                }
+        }
+    })
+}
+function picture() {
+    clonedCards = [...broadways];
+    removeCard()
+    $('.carousel').carousel('dispose')
+    $('.carousel').carousel({
+        interval: 1200
+    })
+    let theseDisappear = document.getElementsByClassName("start");
+        for (let thing of theseDisappear) {
+           thing.style.display = "none";
+    }
+    $('.carousel').on('slid.bs.carousel', function (e) {
+        let num_cards = $('.carousel-item').length
+        if ( (num_cards - 1) === e.to) {
+            $('.carousel-inner').empty();
+            let fiftyTwoAppear = document.getElementsByClassName("containerA");
+                for (let stuff of fiftyTwoAppear) {
+                    stuff.style.display = "block";
+                }
+        }
+    })
+}
+
 function full() {
     clonedCards = [...deckOfCards];
     removeCard()
+    $('.carousel').carousel('dispose')
+    $('.carousel').carousel({
+        interval: 3600
+    })
     let theseDisappear = document.getElementsByClassName("start");
-    for (let thing of theseDisappear) {
-        thing.style.display = "none";
+        for (let thing of theseDisappear) {
+            thing.style.display = "none";
     }
-
+    $('.carousel').on('slid.bs.carousel', function (e) {
+        let num_cards = $('.carousel-item').length
+        if ( (num_cards - 1) === e.to) {
+            $('.carousel-inner').empty();
+            let fiftyTwoAppear = document.getElementsByClassName("containerA");
+                for (let stuff of fiftyTwoAppear) {
+                    stuff.style.display = "block";
+                }
+        }
+    })
 }
 
-removeCard()
+// function train() {
+//     $('#train').carousel()
+//     console.log('hi')
+//     let theseDisappear = document.getElementsByClassName("start");
+//     for (let thing of theseDisappear) {
+//         thing.style.display = "none";
+//     }
+//     $('.carousel-inner').empty();
+//     $('#train').carousel()
+//     console.log('hey')
+// }
+// removeCard()
+
 
 function removeCard() {
     //remove a random card
@@ -70,7 +260,7 @@ function removeCard() {
     }
 
     // reinitialize carousel
-    $('.carousel').carousel()
+    // $('.carousel').carousel()
 }
 
 function shuffle(arr) {
@@ -93,13 +283,6 @@ function createCard(img_src) {
     $('.carousel-inner').append(card);
 }
 
-function noShowCarousel() {
-    if ($('.carousel-item').length === 11) {
-        $('.carousel').carousel('dispose');
-        $('.carousel-inner').empty();
-    }
-}
-
 
 $('.carousel').on('slid.bs.carousel', function (e) {
     let num_cards = $('.carousel-item').length
@@ -113,7 +296,7 @@ $('.carousel').on('slid.bs.carousel', function (e) {
 })
 
 
-//
+
 function guessCard(card) {
     console.log(card);
     if (missingCard == card) {
@@ -125,9 +308,9 @@ function guessCard(card) {
     }
 }
 
-
-
-
+function restart() {
+    location.reload(true);
+}
 
 
 
