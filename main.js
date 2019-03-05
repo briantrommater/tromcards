2d24e27e8bcf2f38806adf9e18400ca418fbe223
brian.trommater <eightysixhearts@yahoo.com>
2019-03-05 14:03:14 -0500

    did someone step on a duck?!

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 
-//these are arrays holding the 9 games
+//these are arrays holding the 12 games
 let deckOfCards = ['2C', '2H', '2S', '2D', '3C', '3H', '3S', '3D', '4C', '4H', '4S', '4D',
 '5C', '5H', '5S', '5D', '6C', '6H', '6S', '6D','7C', '7H', '7S', '7D', '8C', '8H', '8S', '8D',
 '9C', '9H', '9S', '9D', '10C', '10H', '10S', '10D', 'JC', 'JH', 'JS', 'JD', 'QC', 'QH',
@@ -378,7 +378,7 @@ $('.carousel').on('slid.bs.carousel', function (e) {
 function hourlySwitch(card) {
     let date = new Date();
     let hour = date.getHours();
-    if (hour > 19 || hour < 5) {
+    if (hour > 11 || hour < 5) {
         guessCard(card);
     }
     else {
